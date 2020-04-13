e0cafa665b3a5fc42ac2aea1a1904b91c99d8a71
Masayuki Ota <masota@microsoft.com>
2020-04-13 14:19:56 +0900

    Add test project

---
+++ MsTeamsCall.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace MsTeamsCall.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            string test = "test";
+
+            // Assert
+            Assert.Equal(test,"test");
+        }
+    }
+}

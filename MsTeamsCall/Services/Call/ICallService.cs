825b95667adb17e6e6f2583022e37857a6d76f0a
Masayuki Ota <masota@microsoft.com>
2020-04-24 13:48:55 +0900

    add interface

--- MsTeamsCall/Services/Call/ICallService.cs
+++ MsTeamsCall/Services/Call/ICallService.cs
@@ -6,5 +6,6 @@ namespace CseSample.Services
     {
         Task<bool> StartGroupCallWithSpecificMembers(string[] userIds, string tenantId, string accessToken);
         Task<bool> JoinExistingOnlineMeeting(string userId, Meeting meetingInfo, string accessToken);
+        Task<bool> InviteUserToOnlineMeeting(string userId, string tenantId, string callId, string accessToken);
     }
 }
